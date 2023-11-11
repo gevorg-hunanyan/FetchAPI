567add2c9b61eeb7bc5d2c20d34d298091e52d76
Gevorg Hunanyan <gevorg.hunanyan.999@gmail.com>
2023-11-11 17:44:13 +0400

    create button page

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+document.getElementById('dogs').addEventListener('click', function() {
+    window.location.href = 'folder1/page1.html';
+});
+
+document.getElementById('books').addEventListener('click', function() {
+    window.location.href = 'folder2/page2.html';
+});
+
+document.getElementById('films').addEventListener('click', function() {
+    window.location.href = 'folder3/page3.html';
+});
+*/
+
+document.getElementById('countries').addEventListener('click', function() {
+    window.location.href = 'countries/index.html';
+});
