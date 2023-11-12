60b0e154f3d099973baf18cabebbc31fbb33f435
kazrias <ka6rias@gmail.com>
2023-11-12 16:26:16 +0400

    Create createList function

--- films/js/main.js
+++ films/js/main.js
@@ -1,7 +1,28 @@
-const baseUrl = 'https://api.jikan.moe/v4/top/anime';
-
+function createList(json) {
+  const list = document.createElement('div');
+  list.classList.add('anime-list')
+  json.data.forEach(anime => {
+    const genres = anime.genres.map(({ name }) => name).join(', ')
+    const newItem = `
+      <div class="anime-list__item">
+      <img class="anime-list__item-img" src="${anime.images.webp.large_image_url}"
+        alt="anime-img">
+      <div class="anime-list__item-hover">
+        <h3 class="anime-list__item-title">${anime.title}</h3>
+        <div class="anime-list__item-descr">
+          <p class="anime-list__item-genres">Genres: ${genres}</p>
+          <p class="anime-list__item-text">${anime.synopsis}</p>
+        </div>
+        <a class="anime-list__item-link" href="">More</a>
+      </div>
+        </div>`
+    list.insertAdjacentHTML("beforeend", newItem)
+  })
+  return list
+}
 
 async function fetchTopAnimes() {
+  const baseUrl = 'https://api.jikan.moe/v4/top/anime';
   const filterType = 'airing';
   const limit = 20;
   const url = new URL(baseUrl);
@@ -9,24 +30,21 @@ async function fetchTopAnimes() {
   url.searchParams.append('limit', limit);
   const response = await fetch(url);
   const json = await response.json();
-  const list = document.querySelector('.anime-list');
-  json.data.forEach(anime => {
-    const genres = anime.genres.map(({ name }) => name).join(', ')
-    const newItem = `<div class="anime-list__item">
-    <img class="anime-list__item-img" src="${anime.images.webp.large_image_url}"
-      alt="anime-img">
-    <div class="anime-list__item-hover">
-      <h3 class="anime-list__item-title">${anime.title}</h3>
-      <div class="anime-list__item-descr">
-        <p class="anime-list__item-genres">Genres: ${genres}</p>
-        <p class="anime-list__item-text">${anime.synopsis}</p>
-      </div>
-      <a class="anime-list__item-link" href="">More</a>
-    </div>
-  </div>`
-
-    list.insertAdjacentHTML('beforeend', newItem)
-  })
-  console.log(json.data)
+  const container = document.querySelector('.container--anime');
+  const list = createList(json);
+  container.append(list)
 }
 fetchTopAnimes()
+
+
+const search = document.querySelector('.search-form__input');
+search.addEventListener('keydown', async (e) => {
+  if (e.code === 'Enter') {
+    e.preventDefault();
+    const response = await fetch(`https://api.jikan.moe/v4/anime?q=${e.target.value}`)
+    const json = await response.json();
+    const container = document.querySelector('.container--anime');
+    const list = createList(json)
+    container.replaceChildren(list)
+  }
+})
