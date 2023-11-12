4cecc642ecdc348b7720eb5b59d991f4ceb7f564
kazrias <ka6rias@gmail.com>
2023-11-12 09:31:01 +0400

    Add margin item-genres

--- films/js/main.js
+++ films/js/main.js
@@ -1,29 +1,32 @@
 const baseUrl = 'https://api.jikan.moe/v4/top/anime';
-const filterType = 'airing';
-const limit = 20;
-const url = new URL(baseUrl);
-url.searchParams.append('filter', filterType);
-url.searchParams.append('limit', limit);
 
-async function fetchAnimes() {
+
+async function fetchTopAnimes() {
+  const filterType = 'airing';
+  const limit = 20;
+  const url = new URL(baseUrl);
+  url.searchParams.append('filter', filterType);
+  url.searchParams.append('limit', limit);
   const response = await fetch(url);
   const json = await response.json();
   const list = document.querySelector('.anime-list');
-
   json.data.forEach(anime => {
+    const genres = anime.genres.map(({ name }) => name).join(', ')
     const newItem = `<div class="anime-list__item">
     <img class="anime-list__item-img" src="${anime.images.webp.large_image_url}"
       alt="anime-img">
     <div class="anime-list__item-hover">
       <h3 class="anime-list__item-title">${anime.title}</h3>
       <div class="anime-list__item-descr">
-        <p >${anime.synopsis}</p>
+        <p class="anime-list__item-genres">Genres: ${genres}</p>
+        <p class="anime-list__item-text">${anime.synopsis}</p>
       </div>
       <a class="anime-list__item-link" href="">More</a>
     </div>
   </div>`
+
     list.insertAdjacentHTML('beforeend', newItem)
   })
   console.log(json.data)
 }
-fetchAnimes()
+fetchTopAnimes()
