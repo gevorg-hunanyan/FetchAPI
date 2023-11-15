e460551561534666acea6f195231cb8418fe8fea
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2023-11-15 05:01:03 +0400

    TheSchema

---
+++ script.js
@@ -0,0 +1,16 @@
+// document.getElementById('dogs').addEventListener('click', function() {
+//     window.location.href = 'folder1/page1.html';
+// });
+
+document.getElementById('books').addEventListener('click', function() {
+    window.location.href = 'Books/index.html';
+});
+
+// document.getElementById('films').addEventListener('click', function() {
+//     window.location.href = 'folder3/page3.html';
+// });
+
+
+// document.getElementById('countries').addEventListener('click', function() {
+//     window.location.href = 'countries/index.html';
+// });
