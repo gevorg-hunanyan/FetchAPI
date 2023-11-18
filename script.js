a374df7de92fb639dc387e1aa81b8125bc534477
Artyom Harutyunyan <ka6rias@gmail.com>
2023-11-18 10:30:24 +0400

    Change paths

--- script.js
+++ script.js
@@ -6,24 +6,10 @@ document.getElementById('dogs').addEventListener('click', function() {
 document.getElementById('books').addEventListener('click', function() {
     window.location.href = 'Books/index.html';
 });
-<<<<<<< HEAD
 
-<<<<<<< HEAD
 document.getElementById('films').addEventListener('click', function() {
     window.location.href = './films/index.html';
 });
-=======
-// document.getElementById('films').addEventListener('click', function() {
-//     window.location.href = 'folder3/page3.html';
-// });
-=======
-*/
-document.getElementById('films').addEventListener('click', function() {
-    window.location.href = './films/index.html';
-});
-
->>>>>>> ee0a436c3e30b67243700e3e60ac826f5f78679b
->>>>>>> main
 
 
 document.getElementById('countries').addEventListener('click', function() {
